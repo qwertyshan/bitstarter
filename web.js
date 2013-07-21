c13aa9163c3fd592c6e1c19007b26df2d57a4e02
Shantanu Rao <rao.shantanu@gmail.com>
2013-07-21 06:19:21 +0000

    HW4_1 changes

--- web.js
+++ web.js
@@ -1,15 +1,17 @@
 var express = require('express');
 var fs = require('fs');
+var htmlfile = "index.html";
 
 var app = express.createServer(express.logger());
 
-var index = fs.readFileSync('index.html');
+// var index = fs.readFileSync('index.html');
 
 app.get('/', function(request, response) {
-  response.send(index.toString('utf-8'));
+    var html = fs.readFileSync(htmlfile).toString();
+    response.send(html);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
